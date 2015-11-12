1c6935d8c9c91e9932dbf1213450ae891a67c1e2
Usuario <Usuario@2DAWPC02>
2015-11-12 13:22:36 +0100

    Version 0.1 Añadida carpeta con html de ejemplos Probando conectividad de scripts Login.php

--- Proyecto_Noticias/index.php
+++ Proyecto_Noticias/index.php
@@ -6,14 +6,36 @@
         <title></title>
     </head>
     <body>
+        <html>
+    <head>
+        <title>Pagina de Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <fieldset>
+                <legend>Formulario de login </legend>
+                <label>Inserta el login
+                    <input type="text" name="login" id="login"/>
+                </label>
+                <label>Inserta el password 
+                    <input type="password" name="password" id="password"/>
+                </label>
+            <input id="Boton_Envio" type="submit" value="Enviar"/>
+            </fieldset>
+        </form>
+    </body>
+
         <?php
        include_once "utils.php";
        
-       Utils::conectaDb();
-       Utils::imprime("imagenes");
-       Utils::imprime("noticias");
-       Utils::imprime("rol");
-       Utils::imprime("usuarios");
+       
+//       Utils::conectaDb();
+//       Utils::imprime("imagenes");
+//       Utils::imprime("noticias");
+//       Utils::imprime("rol");
+//       Utils::imprime("usuarios");
        
         ?>
     </body>
