27a988da4088340dbc08ca1e5ec465283535370e
Usuario <Usuario@2DAWPC02>
2015-11-13 13:32:05 +0100

    Version0.14 Se han echo pruebas de redirigir a otros scripts php

--- Proyecto_Noticias/php/login.php
+++ Proyecto_Noticias/php/login.php
@@ -1,121 +1,76 @@
 <!DOCTYPE html>
 <html>
-
-<head lang="es">
-    <title>Iniciar Sesion</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href=".//css//estilos.css">
+<head>
+  <meta  charset="UTF-8">
+  <title>Login </title>
+  <link href=<link rel="stylesheet" type="text/css" href=".//css//estilos.css"> 
 </head>
 
 <body>
 <?php
-
-/**
- * funcion login.php
- */
-
-    
-    
-print "<p>";print_r($_REQUEST);print "</p>";
-
-    $query_autenticacion = "select Login,Password,rol_id from usuarios where Login=? and Password=?";
-    $query_recup_rol="select rol.nombre from rol where rol.id=?";
-    
-    $login = recoge("login");
-    $password = recoge("pass");
-    $db = conectaDb("noticias","root","");    
-    $result = $db->prepare($query_autenticacion);
-    if ((strcmp($login,"")==0) || (strcmp($password,"")==0)) 
-    {
-        print "<br>".(strcmp($login,""));
-        print "<br>".(strcmp($password,""));
-        print "<p>Error de credenciales</p>\n";
-    } else 
-    {
-        
-    
-        $result->bindParam(1, $login, PDO::PARAM_STR, 20);
-        $result->bindParam(2, $password, PDO::PARAM_STR, 20);           
-        $result->execute();
-
-// ***** comprueba que result tiene algo
-        if (!$result) 
-        {
-            print "<p>Error en la consulta.</p>\n";
-        } 
-        else 
-        {
-            if ($result->rowCount() <= 0) 
+    // Comprobamos si ya se ha enviado el formulario
+    if (isset($_POST['enviar'])) {
+        $usuario = $_POST['login'];
+        $password = $_POST['pass'];
+       
+        if (empty($usuario) || empty($password)) 
+            $error = "Debes introducir un nombre de usuario y una contraseña";
+        else {
+            print "mmm";
+            try 
             {
-                print "<br>No existe ningún usuario con ese nombre";
-            } 
+                $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+                $dsn = "mysql:host=localhost;dbname=noticias";
+                $dwes = new PDO($dsn, "root", "", $opc);
+            }
+            catch (PDOException $e) {
+                die("Error: " . $e->getMessage());
+            }
+
+            // Ejecutamos la consulta para comprobar las credenciales
+            $query_autenticacion = "select Login,Password,rol_id from usuarios where Login=? and Password=?";
+            $resultado = $dwes->prepare($query_autenticacion);
+            $resultado->bindParam(1, $login, PDO::PARAM_STR, 20);
+            $resultado->bindParam(2, $password, PDO::PARAM_STR, 20);           
+            $resultado->execute();
+   /*         $sql = "SELECT usuario FROM usuarios " .
+            "WHERE usuario='$usuario' " .
+            "AND contrasena='" . md5($password) . "'"; */
+            
+            
+            if($resultado = $dwes->query($sql)) 
+            {
+                $fila = $resultado->fetch();
+                if ($fila != null) {
+                    session_start();
+                    $_SESSION['usuario']=$usuario;
+                   
+                    print "<br>Usuario autenticado";
+                    /*recuperar rol usuario
+                     * 
+                     *grabo cookie con id rol.
+                     * mando a pagina noticias.
+                     */
+                    $query_recupera_rol="select rol.nombre from rol where rol.id=?";
+                    $r_rol = $dwes->prepare($recupera_rol);
+                    
+                    //header("Location: productos.php");                    
+            }
             else 
             {
-                //chicha.
-                print "Autenticado";
-                session_start();
-                // Comprobamos si la variable ya existe
-                if (isset($_SESSION['visitas'])) {
-                    $_SESSION['visitas'] ++;
-                } else {
-                    $_SESSION['visitas'] = 0;
-                }
-
-                //0.01 Recuperar rol de usuario
-                $q_recupera_rol;
-                  //0.1 Grabar cookie de usuario 'login' si es nuevo o hay cambios
-                    //0.1.a Si 'login' es usuario grabar cookie tiempo infinito
-                    //0.1.b Si 'login' es otro grabar cookie para 1 mes.
-                //1. 
-                // 1.a Si rol es usuario lanzar y visionar consultas usuario.
-                // 1.b Si rol es profesor lanzar y visionar consultas profesor.
-                
+                // Si las credenciales no son válidas, se vuelven a pedir
+                $error = "Usuario o contraseña no válidos!";
             }
-        }
-        $db = null; //hemos terminado. session_unset.???
-    }
-
-    /**
-     * Función que graba cookies
-     * @param type $nombre ; El nombre de la cookie
-     * @param type $dato   ; Los datos que vas a guardar en la cookie
-     * @param type $duracion ; Lo que deseas que dure.
-     */
-    function graba_cookie($nombre,$dato,$duracion)
-    {
-        setcookie($name,$dato,$duracion);
-    }
-    
-    
-        /**
-         * Función que quita los caracteres especiales de la $var que recupere de ·$_REQUEST
-         * @param Cadena $var ; el nombre de la var que queremos extraer de $_REQUEST.
-         * @return Devuelve un texto sin blancos, ni slashes, ni caracteres especiales.
-         */
-        function recoge($var) {
-            $tmp = (isset($_REQUEST[$var])) ? strip_tags(trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "utf-8"))) : "";
-            if (get_magic_quotes_gpc()) {
-                $tmp = stripslashes($tmp);
+                unset($resultado);
             }
-            //$tmp = recorta($var, $tmp);
-            return $tmp;
+            unset($dwes);
+            $mivar="asdasdasd";
         }
-//
-        /**
-         * FUNCIÓN DE CONEXIÓN CON LA BASE DE DATOS MYSQL
-         * @param cadena $esquema - Esquema al que nos queremos conectar.
-         * @return Un dataset a la BD a la que nos queremos conectar o una excepción.
-         */
-        function conectaDb($esquema, $bduser, $bdpass) {
-            try {
-                $db = new PDO("mysql:host=localhost;dbname=".$esquema, $bduser, $bdpass);
-                $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
-                return($db);
-            } catch (PDOException $e) 
-            {
-               print "<p>Error: No puede conectarse con la base de datos.</p>\n";
-               exit();
-            }
-        }
+   }
 ?>
+<?php
+
+?>
+
 </body>
+</html>
---
+++ Proyecto_Noticias/php/pon_md5.php
@@ -0,0 +1,12 @@
+<?php
+  $usuario = $_POST['login'];
+        $password = $_POST['pass'];
+        
+        print $login."<br>";
+        print md5($password);
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
