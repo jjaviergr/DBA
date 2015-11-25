bf091bfe5d3ee6075a4a0ef9bdef0100a1eef093
jjaviergr1176 <jjaviergr1176@gmail.com>
2015-11-25 15:11:16 +0100

    Inserta, Actualiza y lista usuarios. No tiene CSS. No tiene Jquery Estoy desarrollando el Index.
    Se le ha añadido el esquema y volcado de la ultima versión de la BD.
    Pendiente de revisar los scripts de operaciones de lista de usuarios con
    la BD.

--- Proyecto_Noticias/php/login.php
+++ Proyecto_Noticias/php/login.php
@@ -8,6 +8,9 @@
 
 <body>
 <?php
+
+   include_once 'util.php';
+   
    $bd_url="localhost";
    $esquema="noticias";
    $bd_user="root";
@@ -34,17 +37,15 @@
         {
            $intentado=$_COOKIE['intentado'];        
         }
-            
+        
         if (!$intentado) // probamos con las credenciales guardadas.
         {
-            //Accedio con exito la ultima vez
-            print "<br>Es la 1º vez que accede con este navegador";
            if( autentica($bd_url,$esquema,$bd_user,$bd_pass,$usuario,$password))
            {
                print "<br>Es un usuario con credenciales en buen estado";
                graba_session($bd_url, $esquema, $bd_user, $bd_pass, $usuario, $password);               
-               print "<br>Location: http://$host/proyecto_noticias/index.php";
-//               header("Location: http://$host/proyecto_noticias/index.php");                    
+              
+               header("Location: http://www.google.es");                    
            }                           
            else 
            {
@@ -61,10 +62,11 @@
        print "<br>No tienes cookies , es tu 1º vez";
        usa_formulario(1,$bd_url, $esquema, $bd_user, $bd_pass);           
     }
-        
+    
+    
     function usa_formulario($grabar_cookies,$bd_url, $esquema, $bd_user, $bd_pass)
     {   
-//         print_r($_POST);
+
        // No hay cookies guardadas
        global $host,$uri,$pagina_destino;
        
@@ -74,21 +76,15 @@
           $password = md5($_POST['pass']);
 //          print "<br>$usuario|$password";
           
-//         echo("<br>usa_formulario_llamando a autentica  :".autentica($bd_url, $esquema, $bd_user, $bd_pass, $usuario, $password)."<br>");
           if (autentica($bd_url, $esquema, $bd_user, $bd_pass, $usuario, $password)==1)
           { 
-             //guardo cookies       
-//             print "<br>Usando formulario";
              $longevidad=graba_session($bd_url, $esquema, $bd_user, $bd_pass,$usuario,$password);
              if($grabar_cookies===1)
              {
-//                print "<br>longevidad :$longevidad";
                 $longevidad=  determina_longevidad(determina_rol($bd_url, $esquema, $bd_user, $bd_pass, $usuario, $password));
                 graba_cookies_credenciales($usuario,$password,$longevidad);
-                print "<br>Grabando cookies de credenciales";
              }
-             print "<br>Location: http://$host/proyecto_noticias/index.php";
-//           header("Location: http://$host/proyecto_noticias/index.php"); 
+             header("Location: http://www.google.es"); 
           }
           else
           {
@@ -99,7 +95,7 @@
        {         
           print "Error de credenciales. Prueba otra vez.";
           print_r($_POST);
-    }
+       }
         
     }
        
@@ -135,97 +131,8 @@
         print "<br>Sesión grabada";
     }
     
-    function determina_rol($bd_url,$bd_esquema,$bd_user,$bd_pass,$login,$password)
-    {
-//        print "<br>determina_rol $bd_url,$bd_esquema,$bd_user,$bd_pass,$login,$password";
-        $dwes=conecta_bd($bd_url, $bd_esquema, $bd_user, $bd_pass);
-        
-//        print "<br> usuarioRol : $login - Rolpass : $password <br>";
-        //select Rol_id from usuarios where Login like 'fulano' AND Password like 'e10adc3949ba59abbe56e057f20f883e';
-        $resultado = $dwes->prepare('select Rol_id from usuarios where Login like :login AND Password like :passwd');       
-        $resultado->execute(Array(':login' => $login,':passwd'=>$password));
+   
        
-        $numfilas=$resultado->rowCount();
-        
-//        print "<br>numfilasrol :$numfilas";
-        
-        if($numfilas>0) 
-        {
-            $fila = $resultado->fetch();
-            return($fila['Rol_id']);
-            print "<br>Usuario autenticado";
-        }
-        else 
-        {
-            // Si las credenciales no son válidas, se vuelven a pedir
-            $error = "<br>Fallo al recuperar el Rol";
-            print "$error";
-            return(false);
-        }
-        
-        unset($resultado);
-        
-        cierra_db($dwes);
-    }
-    
-    function cierra_db($bd)
-    {
-        try
-        {
-            unset($bd);
-        }
-        catch (Exception $e)
-        {
-            die("Error cerrando: " . $e->getMessage());
-        }
-    }
-    
-    function conecta_bd($bd_url,$bd_esquema,$bd_user,$bd_pass)
-    {
-//        print "<br>Conecta :$bd_url|$bd_esquema|$bd_user|$bd_pass";
-        try 
-        {
-            $opc = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-            $dsn = "mysql:host=$bd_url;dbname=$bd_esquema";
-            $dwes = new PDO($dsn, $bd_user, $bd_pass, $opc);
-            if (!$dwes) {
-            print "<br>Fallo.Revisa conexión con BD";
-        }
-    }
-        catch (PDOException $e) {
-            print "<br>Excepcion con la BD : $e";
-        }
-        return $dwes;
-    }
-    
-    function autentica($url,$esquema,$bd_user,$bd_pass,$login,$password)
-    {
-        $encontrado=FALSE;
-        
-//        print "<br>Autentica :$url|$esquema|$bd_user|$bd_pass|$login|$password";
-        
-        $dwes=conecta_bd($url,$esquema,$bd_user,$bd_pass);
-        $resultado = $dwes->prepare('select * from usuarios where Login like :login AND Password like :passwd');
-      
-  
-        $resultado->execute(Array(':login' => $login,':passwd'=>$password));
-       
-        
-        $numero_filas = $resultado->rowCount() ; 
-        
-//        print "<br>numero filas :$numero_filas";
-        if ( $numero_filas > 0 )
-        {
-           $encontrado=TRUE;        
-        }
-       
-        
-        unset($resultado);
-        cierra_db($dwes);
-        
-        
-        return($encontrado);
-    }
 ?>
 
 
